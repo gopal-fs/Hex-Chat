47eb952283f80ce8770782a8c49750d5dc2f1a89
gopal-fs <gopalpinapathuni.fs@gmail.com>
2026-02-18 12:08:22 +0530

    Chnages

--- server/middlewares/authToken.js
+++ server/middlewares/authToken.js
@@ -17,9 +17,11 @@ export const authToken = async(req, res, next) => {
   
       const token = authHeader.split(" ")[1];
       
+      console.log(token)
       
       const payload = jwt.verify(token, process.env.SECRET_KEY);
       
+      console.log(payload)
       const findUser=await userModel.findById(payload.userId)
       
       if(!findUser) return res.status(401).json({success:false,message:"Invalid User"})
